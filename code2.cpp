3c4aae303a6337493dfe29d6def8f361279d799a
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-21 18:52:52 +0530

    second commit

---
+++ code2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float marks;
+    cout << "Enter your marks : ";
+    cin >> marks;
+
+    if(marks >= 33) {
+        cout << "pass" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+
+
+    return 0;
+}
