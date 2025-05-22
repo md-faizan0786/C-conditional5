41adf01bb7a43d26efc483a15b40abfca9c32b85
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-22 16:57:35 +0530

    first commit

---
+++ code3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Qs. 1
+
+int main() {
+    int a, b;
+    cout << "enter a :";
+    cin >>a;
+    cout << "enter b :";
+    cin >> b;
+
+    if(a >= b) {
+        cout << "a is larger : "<< a <<endl;
+    } else {
+        cout << "b is larger : " << b << endl;
+    }
+
+
+
+    return 0;
+}
