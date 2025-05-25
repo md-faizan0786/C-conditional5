3694533cc0165d2c5e73a020150a47f1b9293374
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-25 18:17:14 +0530

    7th commit

---
+++ code7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+
+    cout << "enter a : ";
+    cin >> a;
+
+    cout << "enter b : ";
+    cin >> b;
+
+    cout << "enter operator : ";
+    cin >> op;
+
+
+    // Calculator
+
+    
+    if(op == '+') {
+        cout << "a + b = " << (a+b) << endl;
+    } else if(op == '-') {
+        cout << "a - b = " << (a-b) << endl;
+    } else if(op == '*') {
+        cout << "a * b = " << (a*b) << endl;
+    } else if(op == '/') {
+        cout << "a / b = " << (a/b) << endl;
+    } else {
+        cout << "Invalid Operator";
+    }
+
+
+    return 0;
+}
+    
